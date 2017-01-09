fb3705d79b0f4c2a94c791c2a9fbcf26c0c7433a
Aparna v s <aparnavv.22@gmail.com>
2017-01-09 16:05:45 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,23 @@
+import java,.util.*;
+class Student{
+String name;
+int id;
+String adress;
+Student(String n,int i,String a)
+{
+name=n;
+id=i;
+adress=a;
+}
+void display()
+{
+System.out.println("+name");
+System.out.println(+id);
+System.out.println("+adress");
+public Static main(String[] arg)
+}
+{
+Student s=new Student(aparna,12,koramangala);
+s.display();
+}
+}
